940ee35a7aa6822b5a6e63d8d72800c09e67cf4a
ItsTheBrandonC <89541517+itsthebrandonc@users.noreply.github.com>
2024-07-21 21:51:38 -0400

    Expanded on interests and increased package number for first major release

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -4,7 +4,9 @@ export default function Interests() {
   return (
     <div className="aboutInterest">
       <h2>Interests</h2>
-      <p>Brandon enjoys game development and music production.</p>
+      <p>Brandon enjoys game development, filmmaking, and music production as his main hobbies. 
+        Being able to combine computer science with his hobbies is where he really gets invested.
+      </p>
     </div>
   )
 }
