3da4a37131b091827861d33d2306fe31c0e89b06
ItsTheBrandonC <89541517+itsthebrandonc@users.noreply.github.com>
2024-07-21 21:45:33 -0400

    Corrected year of graduation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className="aboutInterest">
       <h2>About</h2>
       <p>Brandon has been working at Camelot 3PL Software since 2021. He graduated from The University of 
-        North Carolina at Charlotte in 2020 with a Bachelor's Degree of Science in Computer Science with a 
+        North Carolina at Charlotte in 2021 with a Bachelor's Degree of Science in Computer Science with a 
         concentration in AI, Robotics, & Gaming.</p>
     </div>
   )
